99dd0eec0d60a60978c4c1d757defb179a215e30
ccvbmg <54725863+ccvbmg@users.noreply.github.com>
2021-06-08 19:47:35 +0200

    d

---
+++ data.js
@@ -0,0 +1,615 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-haupteingang",
+      "name": "Haupteingang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.08671855850986887,
+        "pitch": 0.021206513832186857,
+        "fov": 1.6208872508693097
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19443638273481945,
+          "pitch": 0.008182199139506352,
+          "rotation": 0,
+          "target": "1-balderichstr"
+        },
+        {
+          "yaw": 0.5442262843707901,
+          "pitch": -0.27946906470088706,
+          "rotation": 0,
+          "target": "9-informatikcodeclub"
+        },
+        {
+          "yaw": 0.7172428022339687,
+          "pitch": -0.2600536702436784,
+          "rotation": 0,
+          "target": "10-ipad-raum"
+        },
+        {
+          "yaw": 0.78069706772445,
+          "pitch": 0.278943916601218,
+          "rotation": 0,
+          "target": "2-sportgelnde"
+        },
+        {
+          "yaw": 2.310887118110818,
+          "pitch": -0.022518690509572892,
+          "rotation": 0,
+          "target": "11-aula"
+        },
+        {
+          "yaw": -0.6238504582358573,
+          "pitch": 0.03569651183772571,
+          "rotation": 0,
+          "target": "7-lernbar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3535043081047782,
+          "pitch": 0.43779360952301083,
+          "title": "Herzlich willkommen!",
+          "text": "Viel Spaß beim 360°-View durch unser schönes GAG."
+        }
+      ]
+    },
+    {
+      "id": "1-balderichstr",
+      "name": "Balderichstr.",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.3015485502551591,
+        "pitch": 0.03491819482777814,
+        "fov": 1.673347415788025
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6666634762592807,
+          "pitch": -0.01388945730680291,
+          "rotation": 0,
+          "target": "0-haupteingang"
+        },
+        {
+          "yaw": 1.9471290578100238,
+          "pitch": -0.027264829866854967,
+          "rotation": 0,
+          "target": "5-innerer-schulhof-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sportgelnde",
+      "name": "Sportgelände",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.237665249437118,
+        "pitch": -0.20430958930229082,
+        "fov": 1.673347415788025
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9793876894030014,
+          "pitch": 0.002889823156802862,
+          "rotation": 0,
+          "target": "0-haupteingang"
+        },
+        {
+          "yaw": 0.4481157210060065,
+          "pitch": 0.15751610023057339,
+          "rotation": 0,
+          "target": "3-teichbecken"
+        },
+        {
+          "yaw": 1.963989404941687,
+          "pitch": 0.009733996267890177,
+          "rotation": 0,
+          "target": "4-auenbereich-spielgerte"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6037342044305607,
+          "pitch": 0.2264059727391654,
+          "title": "Sportgelände",
+          "text": "Hier können sich unsere Schülerinnen und Schüler bei einer Runde Fuß- oder Basketball auspowern."
+        }
+      ]
+    },
+    {
+      "id": "3-teichbecken",
+      "name": "Teichbecken",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.21734821482383282,
+        "pitch": 0.2219738585327775,
+        "fov": 1.6208872508693097
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8653382186712566,
+          "pitch": 0.06711931650060876,
+          "rotation": 0,
+          "target": "2-sportgelnde"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.43790205147740835,
+          "pitch": 0.47686936107487554,
+          "title": "Teichbecken",
+          "text": "Um unser Teichbecken herum lassen sich einige Tierarten beobachten. Unter anderem hat sich hier eine Entengruppe niedergelassen, die sich bei uns heimisch fühlt."
+        }
+      ]
+    },
+    {
+      "id": "4-auenbereich-spielgerte",
+      "name": "Außenbereich Spielgeräte",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.7968975219713013,
+        "pitch": -0.050376543868344115,
+        "fov": 1.6208872508693097
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4585773388165588,
+          "pitch": 0.011782218343384443,
+          "rotation": 0,
+          "target": "6-innerer-schulhof-2"
+        },
+        {
+          "yaw": -0.7008938084028244,
+          "pitch": 0.018972582131629778,
+          "rotation": 0,
+          "target": "2-sportgelnde"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5938847772092082,
+          "pitch": 0.42386187267580056,
+          "title": "Außenbereich Spielgeräte",
+          "text": "Die Spielgeräte des Schulhofes sind gerade bei unseren 5er-Klassen beliebt. Nach der Bewegung, sind sie wieder fit für den kommenden Unterricht.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "5-innerer-schulhof-1",
+      "name": "Innerer Schulhof 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.6208872508693097
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6767841742010603,
+          "pitch": -0.4455861021603269,
+          "rotation": 0,
+          "target": "9-informatikcodeclub"
+        },
+        {
+          "yaw": 0.307634214807619,
+          "pitch": -0.03466021578607581,
+          "rotation": 0,
+          "target": "1-balderichstr"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-innerer-schulhof-2",
+      "name": "Innerer Schulhof 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5593515888921452,
+        "pitch": 0.005722988750809677,
+        "fov": 1.6208872508693097
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9397182749501543,
+          "pitch": 0.02787066444790831,
+          "rotation": 0,
+          "target": "5-innerer-schulhof-1"
+        },
+        {
+          "yaw": -0.4214900127128445,
+          "pitch": -0.0072124123079362334,
+          "rotation": 0,
+          "target": "1-balderichstr"
+        },
+        {
+          "yaw": -2.32603195782913,
+          "pitch": -0.3032507909423874,
+          "rotation": 0,
+          "target": "10-ipad-raum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.13790879570160008,
+          "pitch": 0.2738440727167273,
+          "title": "Innerer Schulhof 2",
+          "text": "Ein weiterer Blick auf unseren ruhig gelegenen inneren Schulhof. Ob Tischtennis oder eine gute Unterhaltung auf unserer \"Sitzschlange\". Jeder nach seiner Laune."
+        }
+      ]
+    },
+    {
+      "id": "7-lernbar",
+      "name": "LernBar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.3406323485810887,
+        "pitch": 0.45502921923954,
+        "fov": 1.673347415788025
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6187455270721731,
+          "pitch": 0.06612304558723459,
+          "rotation": 0,
+          "target": "8-innenhof-lernbar"
+        },
+        {
+          "yaw": 1.743764645328704,
+          "pitch": -0.05940842107918698,
+          "rotation": 0,
+          "target": "0-haupteingang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.017480031853009,
+          "pitch": 0.1330365887840781,
+          "title": "LernBar",
+          "text": "Die LernBar versorgt unsere Schülerinnen und Schüler mit allem, was sie für das selbstständige Lernen und in Gruppen benötigen. Eine ständige Aufsicht unterstützt die Lernenden mit 50 iPads, 12 Macbooks sowie Know-How bei der Bedienung von 2 hochmodernen Smartboards."
+        }
+      ]
+    },
+    {
+      "id": "8-innenhof-lernbar",
+      "name": "Innenhof LernBar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.008418997373295767,
+        "pitch": 0.47702439820575826,
+        "fov": 1.673347415788025
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11862285277031503,
+          "pitch": 0.04975795773080982,
+          "rotation": 0,
+          "target": "7-lernbar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6945301943614801,
+          "pitch": 0.5575826736328331,
+          "title": "Innenhof der LernBar",
+          "text": "Unser neu gestalteter Innenhof unserer LernBar lädt zum Lernen und Verweilen an schönen Sonnentagen ein."
+        }
+      ]
+    },
+    {
+      "id": "9-informatikcodeclub",
+      "name": "Informatik/Codeclub",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.176744385814491,
+        "pitch": 0.2955895872491059,
+        "fov": 1.673347415788025
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1835032627957531,
+          "pitch": 0.18690541891814938,
+          "rotation": 0,
+          "target": "0-haupteingang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.058441941414827,
+          "pitch": 0.03316926486935046,
+          "title": "Informatik/Codeclub",
+          "text": "Einer unserer beiden Informatikräume. Dieser Ort ist das Herz unserer Informatik und des \"CodeClub\". Der \"CodeClub\", der sich gerne als eine große Familie mit über 120 Teilnehmern bezeichnet, trifft sich hier jeden Freitag zum \"coden\" und, um die gemeinsame Zeit zu genießen.<div><br></div><div>Unsere Informatikräume verfügen über leistungsstarke PCs, die allen Anwendungen gerecht werden. Daneben halten wir 150 Notebooks bereit. Falls es im Rahmen der Spieleentwicklung, die bei uns auf Basis der neuesten Unreal-Engine bewerkstelligt wird, einmal mehr Leistung sein darf, verfügen wir über 10 PCs mit höchster Prozessor- und Grafikleistung. So lassen sich die programmierten Games direkt testen. VR? Haben wir in Form einer HTC Valve Index ebenfalls.&nbsp;&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "10-ipad-raum",
+      "name": "iPad-Raum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.848287061061299,
+        "pitch": 0.362371768720644,
+        "fov": 1.6208872508693097
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8110945190544783,
+          "pitch": 0.1331759563703656,
+          "rotation": 0,
+          "target": "0-haupteingang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4685255210354136,
+          "pitch": -0.14759264624179558,
+          "title": "iPad-Raum",
+          "text": "Einer unserer 2 iPad-Räume. Jeder Raum verfügt über einen Schrank mit 30 iPads. Daneben haben wir unsere Montessori-Freiarbeitsräume mit ebenfalls 30 iPads ausgestattet. Mobile iPad-Taschen sind in der LernBar ausleihbar und sind so für jeden Unterricht bei Bedarf leicht einzusetzen. Insgesamt verfügen wir derzeit über 250 iPads."
+        }
+      ]
+    },
+    {
+      "id": "11-aula",
+      "name": "Aula",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.0669603818327023,
+        "pitch": 0.03921281638334939,
+        "fov": 1.6208872508693097
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8420753273246433,
+          "pitch": 0.05575941249575678,
+          "rotation": 0,
+          "target": "0-haupteingang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8650019028760596,
+          "pitch": 0.002860290251685882,
+          "title": "Aula",
+          "text": "In unserer Aula genießen wir unter anderem unsere Musikabende, die jeweils vor Weihnachten und vor den Sommerferien stattfinden. Unser künstlerisches Angebot reicht dabei von unserem Orchester, über den Chor, bis hinzu einem Schwarzlichttheater."
+        }
+      ]
+    }
+  ],
+  "name": "GAG 360°",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
